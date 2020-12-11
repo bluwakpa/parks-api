0e9ea8ebbbad924e88b62035cb28aa7285302dff
Makha Blu Wakpa <bluwakpa@Makhas-MacBook-Pro.local>
2020-12-10 21:16:38 -0800

    working

--- index.js
+++ index.js
@@ -1,69 +1,77 @@
-'use strict';
+"use strict";
 
-// put your own value below!
-const apiKey = '6mnOgfhtjsSq6hr1eLObjDVnQzGVH10CbhPcGHL3'; 
-const searchURL = 'https://developer.nps.gov/api/v1/parks?parkCode=acad&api_key=6mnOgfhtjsSq6hr1eLObjDVnQzGVH10CbhPcGHL3';
-
+const apiKey = '6mnOgfhtjsSq6hr1eLObjDVnQzGVH10CbhPcGHL3';
+var searchURL = `https://developer.nps.gov/api/v1/parks?`;
 
 function formatQueryParams(params) {
-  const queryItems = Object.keys(params)
-    .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`)
-  return queryItems.join('&');
+    const queryItems = Object.keys(params)
+    .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`);
+    console.log(queryItems);
+    console.log(queryItems.join('&'));
+    return queryItems.join('&');
+}
+
+function getNationalParks(state, limit=10) {
+    const params = {
+        api_key: apiKey,
+        stateCode: state,
+        limit,
+    }
+    let queryString = formatQueryParams(params);
+    let url = searchURL + queryString;
+    console.log(url);
+    fetch(url) 
+        .then(response => {
+            if(response.ok) {
+                return response.json();
+            }
+        })
+        .then(responseJson => 
+            displayResults(responseJson)
+        )
+        .catch (error => alert('Sorry, please try again') 
+        );
+}
+
+function onSubmit() {
+    $('#form').on('submit', event => {
+        event.preventDefault();
+        $('#target').empty();
+        let state = $('.state-name').val();
+        state.split(" ").join("");
+        let limit = $('.number').val();
+        getNationalParks(state, limit);
+    })
 }
 
 function displayResults(responseJson) {
-  // if there are previous results, remove them
-  console.log(responseJson);
-  $('#results-list').empty();
-  // iterate through the items array
-  for (let i = 0; i < responseJson.items.length; i++){
-    // for each video object in the items 
-    //array, add a list item to the results 
-    //list with the video title, description,
-    //and thumbnail
-    $('#results-list').append(
-      `<li><h3>${responseJson.items[i].snippet.title}</h3>
-      <p>${responseJson.items[i].snippet.description}</p>
-      <img src='${responseJson.items[i].snippet.thumbnails.default.url}'>
-      </li>`
-    )};
-  //display the results section  
-  $('#results').removeClass('hidden');
+    console.log(responseJson);
+    $('#target').empty();
+    let i = 0;
+    for (let i = 0; i < responseJson.data.length; i++) {
+        if(responseJson.data[i].addresses.length === 0) {
+    $('#target').append(`
+    <li>
+        <p>${responseJson.data[i].fullName}</p>
+        <p>${responseJson.data[i].description}</p>
+        <p><a href="${responseJson.data[i].url}">${responseJson.data[i].url}</a></p>
+    </li>
+    `)} else {
+        $('#target').append(`
+    <li>
+        <p>${responseJson.data[i].fullName}</p>
+        <p>${responseJson.data[i].addresses[0].line1}</p>
+        <p>${responseJson.data[i].addresses[0].city}, ${responseJson.data[i].addresses[0].stateCode}, ${responseJson.data[i].addresses[0].postalCode}</p>
+        <p>${responseJson.data[i].description}</p>
+        <p><a href="${responseJson.data[i].url}">${responseJson.data[i].url}</a></p>
+    </li>
+    `)};
+    $('#results').removeClass('hidden');
+    }
 };
 
-function getNationalParks(query, maxResults=10) {
-  const params = {
-    key: apiKey,
-    q: query,
-    part: 'snippet',
-    maxResults,
-    type: 'video'
-  };
-  const queryString = formatQueryParams(params)
-  const url = searchURL + '?' + queryString;
-
-  console.log(url);
-
-  fetch(url)
-    .then(response => {
-      if (response.ok) {
-        return response.json();
-      }
-      throw new Error(response.statusText);
-    })
-    .then(responseJson => displayResults(responseJson))
-    .catch(err => {
-      $('#js-error-message').text(`Something went wrong: ${err.message}`);
-    });
+function handler() {
+    onSubmit();
 }
 
-function watchForm() {
-  $('form').submit(event => {
-    event.preventDefault();
-    const searchTerm = $('#js-search-term').val();
-    const maxResults = $('#js-max-results').val();
-    getNationalParks(searchTerm, maxResults);
-  });
-}
-
-$(watchForm);
+handler();
